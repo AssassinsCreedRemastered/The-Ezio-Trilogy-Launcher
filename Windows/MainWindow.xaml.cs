93ae1b3e8bc3797c3ce9e4d53633f631da3e9f0d
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2023-12-17 11:07:10 +0100

    Update 11 - Added a check to see if the games are actually in the path - Removed some unecessary code

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Serilog;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Management;
 using System.Text;
@@ -15,6 +16,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using The_Ezio_Trilogy_Launcher.Windows;
+using Microsoft.Win32;
+
 
 namespace The_Ezio_Trilogy_Launcher
 {
@@ -26,6 +29,59 @@ namespace The_Ezio_Trilogy_Launcher
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async Task MissingGame(string game)
+        {
+            try
+            {
+                OpenFileDialog FileDialog = new OpenFileDialog();
+                switch (game)
+                {
+                    case "ACBSP": // ACB
+                        FileDialog.Filter = "Executable Files|ACBSP.exe";
+                        FileDialog.Title = "Select an Assassins Creed Executable";
+                        if (FileDialog.ShowDialog() == true)
+                        {
+                            App.ACBPath = System.IO.Path.GetDirectoryName(FileDialog.FileName);
+                            using (StreamWriter sw = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments) + @"\Assassin's Creed - The Ezio Trilogy Remastered\ACBPath.txt"))
+                            {
+                                sw.WriteLine(App.ACBPath);
+                            };
+                        }
+                        break;
+                    case "ACRSP": // ACR
+                        FileDialog.Filter = "Executable Files|ACRSP.exe";
+                        FileDialog.Title = "Select an Assassins Creed Executable";
+                        if (FileDialog.ShowDialog() == true)
+                        {
+                            App.ACRPath = System.IO.Path.GetDirectoryName(FileDialog.FileName);
+                            using (StreamWriter sw = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments) + @"\Assassin's Creed - The Ezio Trilogy Remastered\ACRPath.txt"))
+                            {
+                                sw.WriteLine(App.ACRPath);
+                            };
+                        }
+                        break;
+                    default: // AC2
+                        FileDialog.Filter = "Executable Files|AssassinsCreedIIGame.exe";
+                        FileDialog.Title = "Select an Assassins Creed Executable";
+                        if (FileDialog.ShowDialog() == true)
+                        {
+                            App.AC2Path = System.IO.Path.GetDirectoryName(FileDialog.FileName);
+                            using (StreamWriter sw = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments) + @"\Assassin's Creed - The Ezio Trilogy Remastered\AC2Path.txt"))
+                            {
+                                sw.WriteLine(App.AC2Path);
+                            };
+                        }
+                        break;
+                }
+                await Task.Delay(1);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "");
+                return;
+            }
         }
 
         // Events/Buttons
@@ -63,17 +119,27 @@ namespace The_Ezio_Trilogy_Launcher
         /// <summary>
         /// Opens Assassin's Creed 2 launcher and hides the main launcher
         /// </summary>
-        private void OpenACII_Click(object sender, RoutedEventArgs e)
+        private async void OpenACII_Click(object sender, RoutedEventArgs e)
         {
             Log.Information("Opening Assassin's Creed 2 Launcher");
             try
             {
+                // Checks if AC2Path exists
                 if (App.AC2Path != null)
                 {
-                    AssassinsCreed2 ac2 = new AssassinsCreed2();
-                    this.Visibility = Visibility.Hidden;
-                    ac2.ShowDialog();
-                    this.Visibility = Visibility.Visible;
+                    // Checks if there's something in AC2Path
+                    if (System.IO.File.Exists(App.AC2Path + @"\AssassinsCreedIIGame.exe"))
+                    {
+                        AssassinsCreed2 ac2 = new AssassinsCreed2();
+                        this.Visibility = Visibility.Hidden;
+                        ac2.ShowDialog();
+                        this.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        System.Windows.MessageBox.Show("Game not found.\nA window will now open asking you to reselect ACBSP.exe");
+                        await MissingGame("AssassinsCreedIIGame");
+                    }
                 } else
                 {
                     MessageBox.Show("Assassin's Creed 2 Remaster is not installed.");
@@ -90,17 +156,27 @@ namespace The_Ezio_Trilogy_Launcher
         /// <summary>
         /// Opens Assassin's Creed: Brotherhood launcher and hides the main launcher
         /// </summary>
-        private void OpenACB_Click(object sender, RoutedEventArgs e)
+        private async void OpenACB_Click(object sender, RoutedEventArgs e)
         {
             Log.Information("Opening Assassin's Creed Brotherhood Launcher");
             try
             {
+                // Checks if ACBPath exists
                 if (App.ACBPath != null)
                 {
-                    AssassinsCreedBrotherhood acb = new AssassinsCreedBrotherhood();
-                    this.Visibility = Visibility.Hidden;
-                    acb.ShowDialog();
-                    this.Visibility = Visibility.Visible;
+                    // Checks if there's something in ACBPath
+                    if (System.IO.File.Exists(App.ACBPath + @"\ACBSP.exe"))
+                    {
+                        AssassinsCreedBrotherhood acb = new AssassinsCreedBrotherhood();
+                        this.Visibility = Visibility.Hidden;
+                        acb.ShowDialog();
+                        this.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        System.Windows.MessageBox.Show("Game not found.\nA window will now open asking you to reselect ACBSP.exe");
+                        await MissingGame("ACBSP");
+                    }
                 }
                 else
                 {
